cda1cc2cd5f54b2e54ac008caf80d7580a6b8e03
Benjamin Bilotta <s3838761@student.rmit.edu.au>
2023-11-20 20:39:57 +1100

    Adds JSON to template and starts work on json to teams

--- src/main/java/org/bot/commands/slash/LeagueRegistration.java
+++ src/main/java/org/bot/commands/slash/LeagueRegistration.java
@@ -3,6 +3,7 @@ package org.bot.commands.slash;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -12,13 +13,23 @@ import net.dv8tion.jda.api.interactions.components.selections.StringSelectMenu;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
+import net.dv8tion.jda.api.utils.AttachedFile;
+import net.dv8tion.jda.api.utils.FileUpload;
 import org.bot.converters.Database;
+import org.bot.converters.JsonConverter;
+import org.bot.models.Team;
 import org.bot.scripts.CommandLogger;
+import org.bot.scripts.RegistrationMessage;
 import org.bot.scripts.ReplyEphemeral;
+import org.bot.scripts.Roles;
+import org.json.simple.parser.ParseException;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -155,5 +166,53 @@ public class LeagueRegistration {
         }
 
         return stringBuilder.toString();
+    }
+
+    public void teamTemplateEvent() {
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle("Team Template JSON")
+                .addField("Notes:", "- Slap ID are option fields and are not required.\n" +
+                        "- A minimun of 2 players and a max of 4 must be entered.", false)
+                .setFooter("If you have any questions of issues feel free to reach out to a League Coordinator",
+                        event.getGuild().getIconUrl());
+
+        event.replyEmbeds(builder.build())
+                .addFiles(FileUpload.fromData(new File("src/main/resources/team-template.json"))).queue();
+    }
+
+    public void teamTemplateUploadEvent() {
+        Message.Attachment file = event.getOptionsByType(OptionType.ATTACHMENT).get(0).getAsAttachment();
+
+        if (!file.getFileExtension().equalsIgnoreCase("json")) {
+            replyEphemeral.sendThenDelete("Invalid file type. Please upload a .json file instead.",
+                    10, TimeUnit.SECONDS);
+            return;
+        }
+
+        try {
+            InputStream inputStream = file.getProxy().download().get();
+            StringBuilder textBuilder = new StringBuilder();
+            try (Reader reader = new BufferedReader(new InputStreamReader
+                    (inputStream, StandardCharsets.UTF_8))) {
+                int c = 0;
+                while ((c = reader.read()) != -1) {
+                    textBuilder.append((char) c);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(textBuilder.toString());
+            JsonConverter jsonConverter = new JsonConverter();
+            Team newTeam = jsonConverter.jsonToTeam(textBuilder.toString());
+
+            new RegistrationMessage(event.getJDA()).leagueTeam(newTeam,
+                    new Roles(event.getGuild()).getTeamRoleField(newTeam.name()));
+
+            replyEphemeral.sendThenDelete("Valid JSON! Team registration now awaiting approval.", 10,
+                    TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | IOException | ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/main/java/org/bot/converters/JsonConverter.java
@@ -0,0 +1,14 @@
+package org.bot.converters;
+
+import com.google.gson.Gson;
+import org.bot.models.Team;
+
+import java.io.*;
+
+public class JsonConverter {
+    public Team jsonToTeam(String json) throws IOException {
+        Gson gson = new Gson();
+
+        return null;
+    }
+}
