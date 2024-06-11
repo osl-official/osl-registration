a03378fd24e06fe7b6e5ef2659cde3aa93d19008
forgehehe <benbilotta@outlook.com>
2024-06-11 22:35:48 +1000

    Refactors code to be able to be run on a server via a jar

--- src/main/java/org/bot/scripts/MessageListener.java
+++ src/main/java/org/bot/scripts/MessageListener.java
@@ -6,9 +6,16 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class MessageListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (!event.getAuthor().isBot() && event.getMessage().toString().toLowerCase().contains("boktay")) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+        if (event.getMessage().toString().toLowerCase().contains("boktay")) {
             event.getMessage().delete().queue();
             event.getChannel().sendMessage("Hey! Don't say that he doesn't like it!!!").queue();
+        } else if (event.getMessage().toString().toLowerCase().contains("shourat")) {
+            event.getMessage().reply("shouni").queue();
+        } else if (event.getMessage().toString().toLowerCase().contains("shouni")) {
+            event.getMessage().reply("shourat").queue();
         }
     }
 }
